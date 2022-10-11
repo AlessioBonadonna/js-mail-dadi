e0f7e41ae1f5e782be60109b8184e5c11e7d0446
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-10-11 16:01:09 +0200

    mail input end

--- js/script.js
+++ js/script.js
@@ -9,16 +9,39 @@
  se e valida stampo in console email valida 
  invece stampo in console email non valida 
 */
-var emailUtente=prompt("Inserisci la tua email");
-console.log(emailUtente);
-let emailValide =["mail1@mail.com","mail2@mail.com","mail3@mail.com"];
-for(i=0;i<emailValide.length;i++){
-    if(emailUtente==emailValide[i]){
-        let login=true;
-    }}
+// var emailUtente=prompt("Inserisci la tua email");
+// console.log(emailUtente);
+// let emailValide =["mail1@mail.com","mail2@mail.com","mail3@mail.com"];
+// for(i=0;i<emailValide.length;i++){
+//     if(emailUtente==emailValide[i]){
+//         let login=true;
+//     }}
 
-    if(login=true){
-        console.log("email non valida ");
-    }else{
-        console.log("email non valida ");
+//     if(login=true){
+//         console.log("email non valida ");
+//     }else{
+//         console.log("email non valida ");
+//     }
+
+
+const emailValide = ["mail1@mail.com", "mail2@mail.com", "mail3@mail.com","pippo" ,"pippo1@mail.com"];
+let esito = document.querySelector('.esito')
+let btn = document.getElementById("login");
+btn.addEventListener("click", function () {
+    let emailUtente = document.getElementById("mail").value;
+    for (i = 0; i < emailValide.length; i++) {
+        console.log(emailUtente);
+
+        if (emailUtente == emailValide[i]) {
+            esito.innerHTML = "Puoi accedere"
+            break;
+        }
+
+        else if (emailUtente == "") {
+            esito.innerHTML = "Inserisci l'Email"
+        }
+        else {
+            esito.innerHTML = "Email non valida"
+        }
     }
+})
